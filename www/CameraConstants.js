077fa9b6f6d7c1e1b240ee0349e1aa7e02f6f92e
Bruno Duarte <brunoaduarte@hotmail.com>
2013-11-12 19:09:18 -0200

    .

--- www/CameraConstants.js
+++ www/CameraConstants.js
@@ -1,4 +1,5 @@
-//cordova.define("strider.camera.Camera", function(require, exports, module) {/*
+
+/*
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -51,4 +52,3 @@ module.exports = {
       FRONT: 1
   }
 };
-//});
